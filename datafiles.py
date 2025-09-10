6d52b5e7e6e035e5c98f17d56995681db3e8a814
Nishchya Kataria <nishchyakataria@Nishchyas-MacBook-Air.local>
2025-09-10 09:43:52 +0530

    Adding dataframe code

--- datafiles.py
+++ datafiles.py
@@ -1,4 +1,19 @@
 import pandas as pd
 
-df = pd.read_csv("data.csv")
-print(df.to_string())
+df = pd.read_csv("data.csv", index_col="Name")
+
+# SELECTION BY COLUMN
+#print(df.to_string())
+#print(df["Name"].to_string())
+#print(df[["Name", "Height"]].to_string())
+
+# SELECTION BY ROW/S
+#df = df.set_index("Name", drop=False)
+#print(df.loc["Charizard":"Blastoise", ["Height", "Weight"]])
+#print(df.iloc[0:11:2])
+pokemon = input("Enter Pokemon name: ")
+
+try:
+    print(df.loc[pokemon])
+except KeyError:
+    print("Pokemon not found")
